a38ea316cd0b45d77eabeecddaf1e7946c772a7e
bensaltanahassan <bensaltana10@gmail.com>
2022-04-25 01:06:32 +0000

    add custom stack

---
+++ lib/widgets/customstack.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomStack extends StatelessWidget {
+  CustomStack({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                blurRadius: 40.0,
+                spreadRadius: 0.0,
+                offset: Offset(
+                  10.0,
+                  10.0,
+                ),
+              ),
+            ],
+          ),
+          width: 250,
+          height: 150,
+          child: Card(
+              elevation: 15,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Product1',
+                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('200 \$',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 24)),
+                        IconButton(
+                          icon: Icon(FontAwesomeIcons.heart, color: Colors.red),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )),
+        ),
+        Positioned(
+          child: Image.network(
+            'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+            height: 100,
+            width: 100,
+          ),
+          top: -40,
+          left: 140,
+        ),
+      ],
+    );
+  }
+}
